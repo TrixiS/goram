c5651e7849752e2e8b02ce339bf0106179106094
TrixiS <oficialmorozov@gmail.com>
2025-02-17 22:02:19 +0300

    Add basic long polling implementation

---
+++ pkg/bot/longpolling.go
@@ -0,0 +1,39 @@
+package bot
+
+import (
+	"context"
+
+	"github.com/TrixiS/goram/pkg/types"
+)
+
+// Polls updates via calling Bot.GetUpdates() in a loop
+//
+// Request parameter is used for initial options of getUpdates requests.
+// The returned channel never gets closed.
+// This function does not panic and does not return errors encountered while making requests.
+// If you need to handle/log those errors or set some retry policy, rewrite it yourself.
+func LongPollUpdates(
+	ctx context.Context,
+	bot *Bot,
+	request *types.GetUpdatesRequest,
+) chan types.Update { // TODO: chan []types.Update for better media group handling?
+	c := make(chan types.Update)
+
+	go func() {
+		for {
+			updates, err := bot.GetUpdates(ctx, request)
+
+			if err != nil || len(updates) == 0 {
+				continue
+			}
+
+			request.Offset = updates[len(updates)-1].UpdateID + 1
+
+			for _, u := range updates {
+				c <- u
+			}
+		}
+	}()
+
+	return c
+}
