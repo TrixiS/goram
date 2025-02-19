e29791f78a82bb16946284961f66f3d6e76adcb0
TrixiS <oficialmorozov@gmail.com>
2025-02-19 17:19:13 +0300

    Add cond flood handler

--- flood/handler.go
+++ flood/handler.go
@@ -2,9 +2,13 @@ package flood
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
+type OnFloodFunc func(ctx context.Context, method string, request any, duration time.Duration)
+
+// Interface for flood handlers. See `flood.CondHandler` or `flood.SleepHandler`. Or write one yourself.
 type Handler interface {
 	Enter(
 		ctx context.Context,
@@ -20,13 +24,14 @@ type Handler interface {
 }
 
 // Dummy flood handler. It will just sleep for flood duration.
+// It might be a wrong choice in a highly concurrent environment,
+// because every goroutine that gets flooded will sleep for n amount of seconds.
+// Also, this handler has no separation of api methods. If one method is flooded,
+// any nonflooded method call will be delayed too.
+//
+// See `flood.CondHandler`.
 type SleepHandler struct {
-	OnFlood func(
-		ctx context.Context,
-		method string,
-		request any,
-		duration time.Duration,
-	) // Optional callback that will be called before sleeping
+	OnFlood OnFloodFunc // Optional callback that will be called before sleeping
 }
 
 func (s *SleepHandler) Enter(context.Context, string, any) {}
@@ -44,4 +49,80 @@ func (s *SleepHandler) Handle(
 	time.Sleep(duration)
 }
 
-// TODO: conditional flood handler
+type methodState struct {
+	mu    sync.Mutex
+	cond  *sync.Cond
+	flood bool
+}
+
+// This handler uses sync.Cond for flood handling.
+// It has additional overhead of memory and locking.
+// Flood is handled in separate for each api method.
+type CondHandler struct {
+	onFlood    OnFloodFunc
+	stateMap   map[string]*methodState // key is api method name
+	stateMapMu sync.RWMutex
+}
+
+func NewCondHandler(onFlood OnFloodFunc) *CondHandler {
+	c := CondHandler{onFlood: onFlood, stateMap: make(map[string]*methodState)}
+	return &c
+}
+
+func (c *CondHandler) Enter(ctx context.Context, method string, request any) {
+	c.stateMapMu.RLock()
+	state := c.stateMap[method]
+	c.stateMapMu.RUnlock()
+
+	if state == nil {
+		return
+	}
+
+	state.mu.Lock()
+
+	for state.flood {
+		state.cond.Wait()
+	}
+
+	state.mu.Unlock()
+}
+
+func (c *CondHandler) Handle(
+	ctx context.Context,
+	method string,
+	request any,
+	duration time.Duration,
+) {
+	c.stateMapMu.Lock()
+	state := c.stateMap[method]
+
+	if state == nil {
+		state = &methodState{}
+		state.cond = sync.NewCond(&state.mu)
+		c.stateMap[method] = state
+	}
+
+	c.stateMapMu.Unlock()
+
+	state.mu.Lock()
+
+	if state.flood {
+		state.mu.Unlock()
+		return
+	}
+
+	if c.onFlood != nil {
+		c.onFlood(ctx, method, request, duration)
+	}
+
+	state.flood = true
+	state.mu.Unlock()
+
+	time.Sleep(duration)
+
+	state.mu.Lock()
+	state.flood = false
+	state.mu.Unlock()
+
+	state.cond.Broadcast()
+}
