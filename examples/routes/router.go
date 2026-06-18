cd158b6574c63946fd2375ed5310cdf9f4576709
TrixiS <oficialmorozov@gmail.com>
2026-06-18 08:51:09 +0300

    Improve handlers example

--- examples/routes/router.go
+++ examples/routes/router.go
@@ -11,22 +11,24 @@ import (
 )
 
 func CreateRouter(adminUserID int64) *handlers.Router {
-	checkAdmin := func(id int64) bool {
-		return adminUserID == 0 || adminUserID == id
+	checkAdmin := func(id int64) (bool, error) {
+		return adminUserID == id, nil
 	}
 
-	router := handlers.
-		NewRouter(handlers.RouterOptions{Name: "root"}).
-		FilterMessage(
-			func(ctx context.Context, bot *goram.Bot, message *goram.Message, data handlers.Data) (bool, error) {
-				return checkAdmin(message.From.ID), nil
-			},
-		).
-		FilterCallbackQuery(
-			func(ctx context.Context, bot *goram.Bot, query *goram.CallbackQuery, data handlers.Data) (bool, error) {
-				return checkAdmin(query.From.ID), nil
-			},
-		).
+	rootRouter := handlers.NewRouter(handlers.RouterOptions{Name: "root"})
+	adminGroup := rootRouter.Group(handlers.RouterOptions{Name: "admin"})
+
+	if adminUserID != 0 {
+		adminGroup.
+			FilterMessage(func(ctx context.Context, bot *goram.Bot, message *goram.Message, data handlers.Data) (bool, error) {
+				return checkAdmin(message.From.ID)
+			}).
+			FilterCallbackQuery(func(ctx context.Context, bot *goram.Bot, query *goram.CallbackQuery, data handlers.Data) (bool, error) {
+				return checkAdmin(query.From.ID)
+			})
+	}
+
+	adminGroup.
 		Message(Start, filters.Text("/start")).
 		CallbackQuery(
 			HelloQuery,
@@ -43,5 +45,5 @@ func CreateRouter(adminUserID int64) *handlers.Router {
 			),
 		)
 
-	return router
+	return rootRouter
 }
